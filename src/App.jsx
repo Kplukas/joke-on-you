a16bb43a5f5fa2a33f2f4762ae6967c256d02f92
Kplukas <p.kristupas.p@gmail.com>
2022-12-02 15:19:56 +0200

    Just inmported jokes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
+import MyComponent from './components/Jokes';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        
+        <MyComponent></MyComponent>
       </header>
     </div>
   );
--- src/components/Jokes.jsx
+++ src/components/Jokes.jsx
@@ -1,4 +1,67 @@
-function Joke(){
+import { useState } from "react";
+import { useEffect } from "react";
 
+function MyComponent() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("https://v2.jokeapi.dev/joke/Programming?amount=10")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <ul>
+        {items.jokes.map(jokes => (
+          <li key={jokes.id}>
+            <h3>{jokes.type}</h3>
+            <p className="part">{jokes.joke}{jokes.setup}</p>
+            <p className="twopart">{jokes.delivery}</p>
+            
+          </li>
+        ))}
+      </ul>
+    );
+  }
 }
-export default Joke;
+export default MyComponent;
+
+//if (items.jokes.type === 'single') {
+//        return (
+//        <ul>
+//            {items.jokes.map(jokes => (
+//            <li key={jokes.id}>
+//            <h3>{jokes.type}</h3>
+//            <p>{jokes.joke}</p>
+//            </li>
+//            ))}
+//        </ul>
+//    );
+//    } else if(items.jokes.type === 'twopart'){
+//        return (
+//        <ul>
+//            {items.jokes.map(jokes => (
+//            <li key={jokes.id}>
+//            <h3>{jokes.type}</h3>
+//            <p>{jokes.setup}</p>
+//            <p>{jokes.delivery}</p>
+//            </li>
+//            ))}
+//      </ul>
+//    );
+//    }
